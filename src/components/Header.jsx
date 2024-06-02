f8236b6dd2e94064b30935d40b9e32340c86745b
Miryam Yanit Baranda Moreno <miryamybm@gmail.com>
2024-06-01 22:25:59 -0600

    Update
    Más detallitos

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,12 +2,22 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <header className="text-white text-center py-4">
+    <header className="text-white text-center py-3">
       <div className="container">
-        <h1>Game of Thrones</h1>
-        <p className="lead">
-          Game of Thrones es una serie de televisión de fantasía medieval creada por David Benioff y D. B. Weiss. Está basada en la serie de novelas Canción de hielo y fuego, del autor George R. R. Martin.
-        </p>
+        <div className="row">
+          <div className="col">
+            <img src="src\assets\img\Casa_Stark_escudo.webp" alt="Imagen1" className="img-fluid" style={{ width: '12rem', height: '12rem' }}/>
+          </div>
+          <div className="col">
+            <h1>Game of Thrones</h1>
+            <p className="lead">
+              Game of Thrones es una serie de televisión de fantasía medieval creada por David Benioff y D. B. Weiss. Está basada en la serie de novelas Canción de hielo y fuego, del autor George R. R. Martin.
+            </p>
+          </div>
+          <div className="col">
+            <img src="src\assets\img\Casa_Targaryen_estandarte.webp" alt="Imagen2" className="img-fluid" style={{ width: '12rem', height: '12rem' }} />
+          </div>
+        </div>
       </div>
     </header>
   );
